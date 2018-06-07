de6d83668e3efe160e81b427736eee3a1bfda66b
Stephen Bunn <stephen@bunn.io>
2018-06-07 15:25:59 -0400

    Adding more user tests
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

--- tests/test_user.py
+++ tests/test_user.py
@@ -3,15 +3,28 @@
 
 import os
 from pathlib import Path
-from requests import Session
 
 import pytest
+from requests import Session
+from hypothesis import given
+from hypothesis.strategies import none, just, integers, text, dictionaries, fixed_dictionaries, composite, one_of, sampled_from
 
 from standardfile import constants, exceptions
 from standardfile.user import User, UserAuth
 
 
+@composite
+def error_dict(draw):
+    return draw(fixed_dictionaries({
+        "message": text(),
+        "payload": dictionaries(text(), one_of(text(), integers(), none())),
+        "tag": one_of(none(), sampled_from(list(exceptions.EXCEPTION_MAPPING.keys())))
+    }))
+
+
 def test_init(testing_user):
+    """Tests proper initialization of a user.
+    """
     (email, password) = testing_user
     user = User(email)
     assert isinstance(user.email, str)
@@ -30,11 +43,13 @@ def test_init(testing_user):
 
 
 def test_login(testing_user):
+    """Tests authentication methods of a user.
+    """
     (email, password) = testing_user
     user = User(email)
     assert not user.authenticated
-    assert not user.mfa_required
     assert user.mfa_key is None
+    assert not user.mfa_required
     with pytest.raises(exceptions.AuthRequired):
         user.sync_dir
 
@@ -52,3 +67,50 @@ def test_login(testing_user):
     assert user.mfa_key is None
     assert isinstance(user.sync_dir, Path)
 
+
+def test_init_mfa(testing_user):
+    """Test initialization of user mfa information works.
+    """
+    (email, password) = testing_user
+    user = User.login(email, password)
+    assert callable(user._init_mfa)
+    user._init_mfa()
+    assert user.mfa_key is None
+    assert user.mfa_required == False
+
+
+@given(one_of(none(), error_dict()))
+def test_handle_error(error):
+    """Test handling of error responses works.
+    """
+    assert callable(User._handle_error)
+    if error is None:
+        assert User._handle_error(error) is None
+    elif isinstance(error, dict):
+        error_tag = error.get("tag")
+        if error_tag:
+            with pytest.raises(exceptions.EXCEPTION_MAPPING[error["tag"]]):
+                User._handle_error(error)
+        else:
+            with pytest.raises(exceptions.StandardFileException):
+                User._handle_error(error)
+
+@given(text(), text(), integers(min_value=1, max_value=60000))
+def test_build_keys(password, salt, cost):
+    """Test building of authentication keys works.
+    """
+    assert callable(User._build_keys)
+    built_keys = User._build_keys(password, salt, cost)
+    assert isinstance(built_keys, UserAuth)
+
+
+# @given(just('get'), one_of(text(), sampled_from(list(constants.ENDPOINTS.keys()))))
+# def test_make_request(method, endpoint):
+#     assert callable(User._make_request)
+#     (email, password) = testing_user
+#     user = User.login(email, password)
+#     if endpoint in constants.ENDPOINTS:
+#         pass
+#     else:
+#         with pytest.raises(ValueError):
+#             user._make_request(method, endpoint)
